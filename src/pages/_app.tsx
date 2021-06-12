32ae8b4bcc923848c9139597997511ca9bb84a90
Cano <contact.sasasaky@gmail.com>
2021-06-12 17:09:28 +0900

    remove ssr styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 import { NextPage } from "next";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import { useEffect } from "react";
 import { NoScript } from "../components/no-script/no-script";
 import { ProgressBar } from "../components/progress-bar";
 import "../styles/globals.css";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.getElementById("jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <ThemeProvider
