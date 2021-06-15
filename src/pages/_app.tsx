13c816757ddcfa4a7f212718ab8cb8612e51354b
Cano <contact.sasasaky@gmail.com>
2021-06-15 14:40:25 +0900

    theme change test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import {
+  createMuiTheme,
+  CssBaseline,
+  PaletteType,
+  ThemeProvider,
+} from "@material-ui/core";
 import { NextPage } from "next";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NoScript } from "../components/no-script/no-script";
 import { ProgressBar } from "../components/progress-bar";
 import "../styles/globals.css";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const [theme, setTheme] = useState<PaletteType>("light");
+
   useEffect(() => {
     const jssStyles = document.getElementById("jss-server-side");
     if (jssStyles) {
@@ -14,11 +21,29 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const update = (matches: boolean) => {
+      setTheme(matches ? "dark" : "light");
+    };
+
+    const listener = (event: MediaQueryListEvent) => {
+      update(event.matches);
+    };
+
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    matchMedia.addEventListener("change", listener);
+    update(matchMedia.matches);
+
+    return () => {
+      matchMedia.removeEventListener("change", listener);
+    };
+  }, []);
+
   return (
     <>
       <ThemeProvider
         theme={createMuiTheme({
-          palette: { type: "dark" },
+          palette: { type: theme },
         })}
       >
         <NoScript />
